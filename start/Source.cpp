70315fc9be110d76b9fc8e0a2306ba8a11583e5f
User_2 <User_2@DESKTOP-KF28T0U>
2020-12-16 17:03:13 +0200

    nachalo

--- start/Source.cpp
+++ start/Source.cpp
@@ -4,6 +4,19 @@
 int main()
 {
 	setlocale(LC_CTYPE, "russian");
+	string prev = " ";
+	string cur = "";
+	int pos_rep = 0;
+	while (cin >> cur)
+	{
+		pos_rep++;
+		if (prev == cur)
+		{
+			cout << "Повторение слова " <<
+				cur << " на позиции " << pos_rep << "\n";
+		}
+		prev = cur;
+	}
 	_getch();
 	return 0;
 }
